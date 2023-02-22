0c8de06af9ba563ba49b1ea9a19b872daeac6f79
Nicolás Valiconto <nvaliconto@outlook.com>
2023-02-22 18:55:15 -0300

    delete test

--- main.py
+++ main.py
@@ -70,3 +70,10 @@ def update_movie(id: int, title: str = Body(), overview: str = Body(), year: int
 			item['rating'] = rating
 			item['category'] = category
 			return movies
+
+@app.delete('/movies/{id}', tags=['movies'])
+def delete_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            movies.remove(item)
+            return item
