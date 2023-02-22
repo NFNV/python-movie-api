2dbce060473260c36f608b7a5ee63df1208b37ae
Nicolás Valiconto <nvaliconto@outlook.com>
2023-02-22 20:28:09 -0300

    data type validations

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 app.title = 'FastAPI app'
 app.version = '0.0.1'
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3, max_length=50)
+    overview: str = Field(min_length=3, max_length=150)
+    year: int = Field(le=2023)
+    rating: float
+    category: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "title": "My movie",
+                "overview": "My description",
+                "year": 2023,
+                "rating": 7.0,
+                "category": "action"
+            }
+        }
 
 movies = [
     {
@@ -49,26 +71,28 @@ def get_movies_by_category(category: str):
     return [item for item in movies if item['category'] == category]
 
 @app.post('/movies/', tags=['movies'])
-def create_movie(id: int = Body(), title: str = Body(), overview: str = Body(), year: int = Body(), rating: int = Body(), category: str = Body()):
-    movies.append({
-        'id': id,
-        'title': title,
-        'overview': overview,
-        'year': year,
-        'rating': rating,
-        'category': category
-    })
+# def create_movie(id: int = Body(), title: str = Body(), overview: str = Body(), year: int = Body(), rating: int = Body(), category: str = Body()):
+#     movies.append({
+#         'id': id,
+#         'title': title,
+#         'overview': overview,
+#         'year': year,
+#         'rating': rating,
+#         'category': category
+#     })
+def create_movie(movie: Movie):
+    movies.append(movie)
     return movies
 
 @app.put('/movies/{id}', tags=['movies'])
-def update_movie(id: int, title: str = Body(), overview: str = Body(), year: int = Body(), rating: int = Body(), category: str = Body()):
+def update_movie(id: int, movie: Movie):
 	for item in movies:
 		if item["id"] == id:
-			item['title'] = title
-			item['overview'] = overview
-			item['year'] = year
-			item['rating'] = rating
-			item['category'] = category
+			item['title'] = movie.title
+			item['overview'] = movie.overview
+			item['year'] = movie.year
+			item['rating'] = movie.rating
+			item['category'] = movie.category
 			return movies
 
 @app.delete('/movies/{id}', tags=['movies'])
