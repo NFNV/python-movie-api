369c9a58104610a3470adc1cc4b931f63667608c
Nicolás Valiconto <nvaliconto@outlook.com>
2023-02-22 17:00:36 -0300

    get test

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-app.title = "FastAPI app"
-app.version = "0.0.1"
+app.title = 'FastAPI app'
+app.version = '0.0.1'
 
-@app.get("/", tags=["home"])
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    }
+]
 
+@app.get('/', tags=['home'])
 def message():
-    return "hello world!"
+    return HTMLResponse('<h1>Hello world!</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
