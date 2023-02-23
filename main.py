77e577f09941ec80cb12e5ccb74e600d4cb900ab
Nicolás Valiconto <nvaliconto@outlook.com>
2023-02-23 20:50:16 -0300

    tokens with pyjwt

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI, Body, Path, Query
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
+from jwt_manager import create_token
 
 app = FastAPI(title='FastAPI app', version='0.0.1')
+
+class User(BaseModel):
+    email: str
+    password: str
 
 class Movie(BaseModel):
     id: Optional[int] = None
@@ -47,6 +52,10 @@ movies = [
 @app.get('/', tags=['home'])
 def message():
     return HTMLResponse('<h1>Hello world!</h1>')
+
+@app.post('/login', tags=['auth'])
+def login(user: User):
+    return user
 
 @app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200)
 def get_movies() -> List[Movie]:
